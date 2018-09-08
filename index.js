b86de04b1d095aaa5e83e0819e8f092ca080d50e
shsg <creiss.@uc.cl>
2018-09-08 15:12:58 +0200

    pull

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use(express.static('public'));
 var io= socket(server);
 
 io.on('connection', function(socket){
-  console.log('made socket connection')
+  console.log('made socket connection', socket.id)
 });
