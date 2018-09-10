8852701c9f1831a8d2e75619124d44daa0b09120
jacquelinedebuysroessingh <jacquelinedebuysroessingh@eduroamstud-130-82-250-215.unisg.ch>
2018-09-10 18:38:02 +0200

    wagon-frontend

--- public/chat.js
+++ public/chat.js
@@ -6,9 +6,17 @@ var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     output = document.getElementById('output'),
-    feedback = document.getElementById('feedback');
+    feedback = document.getElementById('feedback'),
+    demo = document.getElementById('demo');
 
-// Emit events
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
 
 btn.addEventListener('click', function(){
   socket.emit('chat', {
@@ -24,10 +32,33 @@ message.addEventListener('keypress', function(){
 
 //Listen for events
 socket.on('chat', function(data){
+  var d = new Date();
+  var x = document.getElementById("demo");
+  var h = addZero(d.getHours());
+  var m = addZero(d.getMinutes());
+  var s = addZero(d.getSeconds());
   feedback.innerHTML = "";
-  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message  + '</p>' + '<p style="font-size:12;font-weight:lighter;">' + h + ":" + m + ":" + s + '</p>';
+
 });
 
 socket.on('typing',function(data){
   feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
+
+message.addEventListener('keypress', function(w){
+  if(w.keyCode == 13){
+    socket.emit('chat',{
+      message: message.value,
+      handle: handle.value
+
+    });
+    message.value = "";
+  }
+});
+
+// to understand this, it means that the function is an
+//argument and therefore cannot function as a function.
+// BUT, because of the addeventlistener, it means make the argument work
+//as a function. so when you keypress, the function will execute itself
+//and emit a message and a handle (name)
