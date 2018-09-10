850756138799c1c8e9425a9d5e45d422f9f45599
stefantari <stefangetsregistered@gmail.com>
2018-09-10 09:53:33 +0200

    Changes until Saturday evening

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000,function(){
-  console.log('listen to requests on port 4000');
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
 });
 
 // Static files
 app.use(express.static('public'));
 
-// Socket setup
-var io = socket (server)
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', function (socket){
 
-io.on("connection", function(){
-  console.log("made socket connection", socket.id)
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
